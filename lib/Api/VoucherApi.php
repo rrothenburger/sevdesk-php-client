c1b5468652425593eeeb8eca3deabf51751dfc31
enerspace <40614728+enerspace@users.noreply.github.com>
2019-09-10 21:17:50 +0200

    Update VoucherApi.php

--- lib/Api/VoucherApi.php
+++ lib/Api/VoucherApi.php
@@ -5426,12 +5426,12 @@ class VoucherApi
             } else {
                 $content = $responseBody->getContents();
                 if ($returnType !== 'string') {
-                    $content = json_decode($content);
+                    $content = json_decode($content, true);
                 }
             }
 
             return [
-                ObjectSerializer::deserialize($content, $returnType, []),
+                $content,
                 $response->getStatusCode(),
                 $response->getHeaders()
             ];
